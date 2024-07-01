d9bc21744e8c938c6965a7f0de5cf3fe616d10c4
Mauro P <maurop123@gmail.com>
2024-07-01 00:32:55 -0600

    add ai

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,14 +1,45 @@
 <template>
-  <h1>Hello from Vercel</h1>
-  This page is:
-  <ul>
-    <li>Rendered to HTML.</li>
-    <li>Interactive. <Counter /></li>
-  </ul>
+  <h1>Hello</h1>
+  <p>
+    {{ state.message }}
+  </p>
+  <textarea
+    cols="40" rows="6"
+    v-model="state.input"
+    placeholder="Write your message for AI"
+  >
+  </textarea>
+  <br><br>
+  <button @click="send('user', state.input)">Send</button>
 </template>
 
-<script>
+<script setup>
+import OpenAI from 'openai'
+import { reactive } from 'vue'
 import Counter from './Counter.vue'
-const components = { Counter }
-export default { components }
+
+const state = reactive({ message: '', input: ''})
+const openai = new OpenAI({
+  apiKey: import.meta.env.OPENAI_APIKEY,
+  organization: '',
+  project: '',
+  dangerouslyAllowBrowser: true,
+})
+/* console.debug('openai', openai) */
+
+async function send(_role, _content) {
+  const completions = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-1106',
+    messages: [{
+      role: _role || 'system',
+      content: _content || 'You are a helpful assistant',
+    }],
+  })
+  /* console.debug('completions', completions) */
+
+  state.message = completions.choices[0].message.content
+  state.input = ''
+}
+
+send()
 </script>
